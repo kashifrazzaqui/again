8b56babeb736b6edd34d3ba69534575a17495a1b
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2014-02-26 16:28:33 +0530

    make all event names uppercase

--- events.py
+++ events.py
@@ -1,23 +1,25 @@
-__author__ = 'kashif'
-
 class ListenerRegister(object):
     def __init__(self):
         self._register = {}
 
     def add_listener(self, event, listener):
+        event = event.upper()
         listeners = self._register.get(event, set())
         listeners.add(listener)
         self._register[event] = listeners
 
     def remove_listener(self, event, listener):
+        event = event.upper()
         listeners = self._register.get(event, None)
         if listeners:
             listeners.remove(listener)
 
     def remove_event(self, event):
+        event = event.upper()
         del self._register[event]
 
     def get_listeners(self, event):
+        event = event.upper()
         return self._register.get(event, set())
 
 class EventSource(ListenerRegister):
