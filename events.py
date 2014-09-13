426521115d62c664dc9139956f092a92705f7795
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2014-09-13 08:48:48 +0530

    Add an example for event usage

--- events.py
+++ events.py
@@ -32,3 +32,26 @@ class EventSource(ListenerRegister):
             each(*args, **kwargs)
 
 
+class TestClass(EventSource):
+    def __init__(self):
+        super(TestClass, self).__init__()
+        print("ready")
+
+    def event_occurs(self):
+        # parameters for fire are 'event name' followed by anything you want to pass to the listener
+        self.fire("big bang event", "what a blast!")
+
+    def simple_listener(self, payload):
+        print("Payload : {0}".format(payload))
+
+
+def demo():
+    t = TestClass()
+
+    # takes an event (any valid python object) and a listener (any valid python function)
+    t.add_listener("big bang event", t.simple_listener)
+
+    t.event_occurs() #when the event is fired in this method, the listener is informed
+
+if __name__ == '__main__':
+    demo()
