d6245fe711c0a6c9572acff6da19a54af41a0df4
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2014-12-12 15:08:11 +0530

    Eventing boilerplate now provides capability to attach a 'catch_all' listener which gets ALL event

--- events.py
+++ events.py
@@ -1,6 +1,7 @@
 class ListenerRegister(object):
     def __init__(self):
         self._register = {}
+        self._catch_alls = set()
 
     def add_listener(self, event, listener):
         event = event.upper()
@@ -8,11 +9,17 @@ class ListenerRegister(object):
         listeners.add(listener)
         self._register[event] = listeners
 
+    def add_catch_all_listener(self, listener):
+        self._catch_alls.add(listener)
+
     def remove_listener(self, event, listener):
         event = event.upper()
         listeners = self._register.get(event, None)
         if listeners:
             listeners.remove(listener)
+
+    def remove_catch_all_listener(self, listener):
+        self._catch_alls.remove(listener)
 
     def remove_event(self, event):
         event = event.upper()
@@ -22,6 +29,10 @@ class ListenerRegister(object):
         event = event.upper()
         return self._register.get(event, set())
 
+    def get_catch_all_listeners(self):
+        return self._catch_alls
+
+
 class EventSource(ListenerRegister):
 
     def __init__(self):
@@ -30,6 +41,8 @@ class EventSource(ListenerRegister):
     def fire(self, event, *args, **kwargs):
         for each in self.get_listeners(event):
             each(*args, **kwargs)
+        for each in self.get_catch_all_listeners():
+            each(event, *args, **kwargs)
 
 
 class TestClass(EventSource):
@@ -39,7 +52,8 @@ class TestClass(EventSource):
 
     def event_occurs(self):
         # parameters for fire are 'event name' followed by anything you want to pass to the listener
-        self.fire("big bang event", "what a blast!")
+        self.fire("big bang", "what a blast!")
+        self.fire("unknown", "Something")
 
     def simple_listener(self, payload):
         print("Payload : {0}".format(payload))
@@ -51,7 +65,11 @@ def demo():
     # takes an event (any valid python object) and a listener (any valid python function)
     t.add_listener("big bang event", t.simple_listener)
 
-    t.event_occurs() #when the event is fired in this method, the listener is informed
+    def catchy(event_name, payload):
+        print('Caught event {}: {}'.format(event_name, payload))
+
+    t.add_catch_all_listener(catchy)
+    t.event_occurs()  # when the event is fired in this method, the listener is informed
 
 if __name__ == '__main__':
     demo()
