bb964199277f934d36c908b2ea89d3a33d691661
nerandell <ankitchandawala@gmail.com>
2014-12-16 18:05:40 +0530

    Using distutils instead of setuptools~

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from distutils.core import setup
 setup(name='PyUtils',
 	version='1.0',
 	description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.',
