730327c9581cdeef9addae5faef5202943cd0ccb
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2014-12-17 12:30:13 +0530

    removed redundant subdir pyutils

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from distutils.core import setup
-setup(name='PyUtils',
+setup(name='again',
 	version='1.0',
-	description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.',
-	packages=['pyutils']
+	description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.', packages=['']
 	)
 
--- statemachine.py
+++ statemachine.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from pyutils.events import EventSource
+from again.events import EventSource
 
 import logging
 
