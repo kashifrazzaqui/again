d28a40249eeb9cdc8598ca4038170b88003c7aed
Kashif Razzaqui <kashif.razzaqui@gmail.com>
2014-12-17 13:34:23 +0530

    changed folder structure so that imported egg works with pycharm correctly

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 setup(name='again',
 	version='1.0',
-	description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.', packages=['']
+        author='Kashif Razzaqui',
+        author_email='kashif.razzaqui@gmail.com',
+        url='https://github.com/kashifrazzaqui/again',
+	description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.',
+        packages=['again']
 	)
 
