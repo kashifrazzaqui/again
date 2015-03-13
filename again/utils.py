b4899fcbe889c7d1643257cda6259d7282d316d9
Ankit Chandawala <ankitchandawala@gmail.com>
2015-03-13 11:26:10 +0530

    Adding natural_sort method

--- again/utils.py
+++ again/utils.py
@@ -1,7 +1,29 @@
 import os, binascii
+import re
+
 
 def unique_hex(byte_length=4):
     return binascii.hexlify(os.urandom(byte_length)).decode()
+
+
+def natural_sort(item):
+    """
+    Sort strings that contain numbers correctly.
+
+    >>> l = ['v1.3.12', 'v1.3.3', 'v1.2.5', 'v1.2.15', 'v1.2.3', 'v1.2.1']
+    >>> l.sort(key=natural_sort)
+    >>> print l
+    "['v1.2.1', 'v1.2.3', 'v1.2.5', 'v1.2.15', 'v1.3.3', 'v1.3.12']"
+    """
+    if item is None:
+        return 0
+
+    def try_int(s):
+        try:
+            return int(s)
+        except ValueError:
+            return s
+    return map(try_int, re.findall(r'(\d+|\D+)', item))
 
 
 if __name__ == '__main__':
