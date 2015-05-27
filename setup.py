0f0cd8ee729de2e64340aac23fbf90ba21e6a927
Ankit Chandawala <ankitchandawala@gmail.com>
2015-05-27 18:17:57 +0530

    Bumping up version number

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
+
 setup(name='again',
-	version='1.2',
-        author='Kashif Razzaqui',
-        author_email='kashif.razzaqui@gmail.com',
-        url='https://github.com/kashifrazzaqui/again',
-	description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.',
-        packages=['again']
-	)
+      version='1.2.1',
+      author='Kashif Razzaqui',
+      author_email='kashif.razzaqui@gmail.com',
+      url='https://github.com/kashifrazzaqui/again',
+      description='Python decorators for type and value checking at runtime. Also, some boilerplate code for making classes that support event registration and firing.',
+      packages=['again']
+)
 
